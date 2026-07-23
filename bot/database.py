d0ec0ef1f435f926f59694b1a40b8ede49e0e799
fanatpzdt <borovichnicely@gmail.com>
2026-07-23 20:44:30 +0300

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -12,7 +12,13 @@ def create_table():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS messages (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id TEXT,
         text TEXT,
+        city TEXT,
+        district TEXT,
+        problem TEXT,
+        duration TEXT,
+        status TEXT DEFAULT 'new',
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     """)
@@ -21,14 +27,37 @@ def create_table():
     conn.close()
 
 
-def save_message(text):
+def save_message(
+    user_id,
+    text,
+    city=None,
+    district=None,
+    problem=None,
+    duration=None
+):
     conn = connect()
     cursor = conn.cursor()
 
-    cursor.execute(
-        "INSERT INTO messages (text) VALUES (?)",
-        (text,)
+    cursor.execute("""
+    INSERT INTO messages
+    (
+        user_id,
+        text,
+        city,
+        district,
+        problem,
+        duration
     )
+    VALUES (?, ?, ?, ?, ?, ?)
+    """,
+    (
+        user_id,
+        text,
+        city,
+        district,
+        problem,
+        duration
+    ))
 
     conn.commit()
     conn.close()
