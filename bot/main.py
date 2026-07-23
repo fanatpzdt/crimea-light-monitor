3761b4bf37ed3bdaa45c71b6ad1fea116c9e80ca
fanatpzdt <borovichnicely@gmail.com>
2026-07-23 20:24:52 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-
+from database import create_table, save_message
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
