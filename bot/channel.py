626d412e7098d27bbf7115c81b11f5990192691b
fanatpzdt <borovichnicely@gmail.com>
2026-07-23 23:42:17 +0300

    Update channel.py

--- bot/channel.py
+++ bot/channel.py
@@ -4,12 +4,13 @@ from config import CHANNEL_ID
 async def publish(application, city, count):
 
     text = (
-        "🚨 Возможное отключение электроэнергии\n\n"
-        f"📍 {city}\n\n"
-        f"Подтвердили: {count} человек"
+        f"🔴 <b>{city}</b>\n\n"
+        f"Нет света\n\n"
+        f"👥 Подтвердили: {count}"
     )
 
     await application.bot.send_message(
         chat_id=CHANNEL_ID,
-        text=text
+        text=text,
+        parse_mode="HTML"
     )
