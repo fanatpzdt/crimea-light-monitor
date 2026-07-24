4def5967daff44d04d46b6a9189bd73ea3766cdd
fanatpzdt <borovichnicely@gmail.com>
2026-07-24 17:32:03 +0300

    Add print statements for count and ALERT_THRESHOLD

--- bot/main.py
+++ bot/main.py
@@ -151,6 +151,8 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         count = get_city_stats(city)
 
+        print("СЧЁТЧИК:", count)
+        print("ЛИМИТ:", ALERT_THRESHOLD)
 
         if status == "no_power":
 
