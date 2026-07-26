b64ac27732e873d5dace5b14159f46ad9eef5298
fanatpzdt <borovichnicely@gmail.com>
2026-07-26 14:20:21 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -88,7 +88,7 @@ async def save_power_report(query, context, city):
     user_id = query.from_user.id
 
 
-        save_report(
+    save_report(
         user_id,
         city,
         status
@@ -99,7 +99,6 @@ async def save_power_report(query, context, city):
         city,
         status
     )
-
 
     count = get_city_stats(city)
 
