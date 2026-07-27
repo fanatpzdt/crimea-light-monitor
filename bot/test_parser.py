fe547f1caa7ae9460412654c9d20c0c5eaee6665
fanatpzdt <borovichnicely@gmail.com>
2026-07-27 11:33:39 +0300

    Update test_parser.py

--- bot/test_parser.py
+++ bot/test_parser.py
@@ -1,1 +1,19 @@
-print("ТЕСТ ПАРСЕРА")
+from parser import get_latest_news, parse_news
+
+
+news = get_latest_news()
+
+print(news)
+
+
+if news:
+
+    text = parse_news(
+        news["url"]
+    )
+
+    print("\n---НОВОСТЬ---\n")
+
+    print(
+        text[:2000]
+    )
